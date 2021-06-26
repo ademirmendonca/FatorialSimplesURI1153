31647c0ddade13fd211047f2927aa4bba9d96b48
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-06-26 15:51:28 -0300

    Add project files.

---
+++ FatorialSimplesURI1153/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FatorialSimplesURI1153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int total = 1;
+            
+            for (int i = 1; i <= n; i++)
+            {
+                total = total * i;
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
